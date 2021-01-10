9d43142075ee9efeb5673064929114eae6ce9353
akira-yp <yog.puripuri@gmail.com>
2021-01-10 23:05:19 +0900

    アカウント登録画面を

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,17 @@
+class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to new_user_path, notice:'アカウントを登録しました'
+    else
+      render :new
+    end
+  end
+  private
+  def user_params
+    params.require(:user).permit(:name,:email,:password)
+  end
+end
