308e14db849dd400a3c3435633d590c65fdf515f
akira-yp <yog.puripuri@gmail.com>
2021-01-10 22:02:49 +0900

    セキュアパスワードの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   validates :email, presence: true, length:{ maximum: 200 ),
     format:{ with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i },
     uniqueness: true
+  before_validation { email.downcase! }
+  has_secure_password
+  validates :password_digest, presence: true, length:{ minimum: 6 }
 end
