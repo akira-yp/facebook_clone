4b938df20980da6d607a0cc27c4c3656c0efeed3
akira-yp <yog.puripuri@gmail.com>
2021-01-30 00:51:49 +0900

    コメント行などを

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,7 +1,6 @@
 class PicturesController < ApplicationController
   before_action :set_all_pictures, only:[:index, :create, :confirm]
   before_action :set_picture, only:[:show, :edit, :update, :destroy]
-
   def index
     @picture = current_user.pictures.new
   end
