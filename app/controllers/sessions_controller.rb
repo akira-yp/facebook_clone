5b613d736ca118395191163ef3c6685f9e6c821a
akira-yp <yog.puripuri@gmail.com>
2021-01-30 18:24:21 +0900

    ログイン画面のエラーメッセージ表示を修正

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,14 +8,14 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       session[:user_id] = user.id
-      redirect_to user_path(user.id)
+      redirect_to user_path(user.id), notice:"ログインしました"
     else
-      flash.now[:danger]="ログイン失敗しました"
+      flash.now[:danger] = "ログインに失敗しました"
       render :new
     end
   end
   def destroy
     session.delete(:user_id)
-    redirect_to new_session_path
+    redirect_to new_session_path, notice:"ログアウトしました"
   end
 end
