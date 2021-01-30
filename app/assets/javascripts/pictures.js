0cf6c88b3cdbc94ebced412c95b190f79c2f9d4a
akira-yp <yog.puripuri@gmail.com>
2021-01-30 20:59:50 +0900

    文字数カウント機能を修正

--- app/assets/javascripts/pictures.js
+++ app/assets/javascripts/pictures.js
@@ -1,4 +1,13 @@
 $(function(){
+  let count = $('.content-form').val().length;
+  let $submit = $('.submit-btn');
+  if (count >= 1 && count <= 500){
+    $submit.children('svg').css('fill','#2F72FF');
+    $submit.removeClass('no-click');
+  }else{
+    $submit.children('svg').css('fill','#8baffc');
+    $submit.addClass('no-click');
+  };
   $(document).on('keyup','.content-form',function(){
     let count = $(this).val().length;
     let $submit = $('.submit-btn');
